47d2040f0e803aed6884995aa7e37c645bb5adb2
Steffan <Fiskk@live.com>
2022-04-11 00:20:51 -0400

    Initial rework commit
    Added filter on difficulty.
    Separated out API call function

--- leetcode-to-slack.py
+++ leetcode-to-slack.py
@@ -1,16 +1,37 @@
+# from logging import _Level
 import requests
 import json
-import os
 from slack import WebClient
 from slack.errors import SlackApiError
-import schedule
-import time
 from random import randrange
 
-def post_problem(all_problems):
+
+# JSON Form
+# "stat": {
+#         "question_id": 12,
+#         "question__article__live": true,
+#         "question__article__slug": "integer-to-roman",
+#         "question__article__has_video_solution": false,
+#         "question__title": "Integer to Roman",
+#         "question__title_slug": "integer-to-roman",
+#         "question__hide": false,
+#         "total_acs": 667835,
+#         "total_submitted": 1126162,
+#         "frontend_question_id": 12,
+#         "is_new_question": false
+#       },
+#       "status": null,
+#       "difficulty": { "level": 2 },
+#       "paid_only": false,
+#       "is_favor": false,
+#       "frequency": 0,
+#       "progress": 0
+
+def post_problem(all_problems, difficulty, type):
+
     slug = all_problems[randrange(len(all_problems))]  # choose random problem
-    webhook_url = ''
-    output = { "text": "This week's warm-up problem: " + "https://leetcode.com/problems/" + slug }
+    webhook_url = 'https://hooks.slack.com/services/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'
+    output = { "text": "Today's " + difficulty + " " + type + " problem:" + "https://leetcode.com/problems/" + slug }
     # post
     slack_response = requests.post(
         webhook_url, data=json.dumps(output),
@@ -22,20 +43,26 @@ def post_problem(all_problems):
             % (slack_response.status_code, slack_response.text)
         )
 
-# get data
-response = requests.get("https://leetcode.com/api/problems/algorithms/")
-data = json.loads(response.content)
+def get_algorithm_problems():
+    # get data
+    response = requests.get("https://leetcode.com/api/problems/algorithms/")
+    data = json.loads(response.content)
 
-# filter-out premium ones, get slugs
-all_problems = []
-for problem in range(len(data["stat_status_pairs"])):
-    if not data["stat_status_pairs"][problem]["paid_only"]:
-        single_problem = data["stat_status_pairs"][problem]["stat"]["question__title_slug"]
-        all_problems.append(single_problem)
+    return data
 
-# schedule job
-schedule.every().monday.at("10:46").do(post_problem, all_problems)
+def get_easy_algorithm_problems(problems):
+    # filter-out premium, medium, and hard problems, get slugs
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
+    all_easy_algorithm_problems = []
+    for problem in range(len(problems["stat_status_pairs"])):
+        difficulty_dict = json.loads(str(problems["stat_status_pairs"][problem]["difficulty"]).replace("'", "\"", 2))
+        if (not problems["stat_status_pairs"][problem]["paid_only"]) and difficulty_dict['level'] == 1:
+            #if data # look for easy questions
+            single_problem = problems["stat_status_pairs"][problem]["stat"]["question__title_slug"]
+            all_easy_algorithm_problems.append(single_problem)
+
+    return all_easy_algorithm_problems
+
+all_algorithm_problems = get_algorithm_problems()
+post_problem(get_easy_algorithm_problems(all_algorithm_problems), 'easy', 'algorithm')
